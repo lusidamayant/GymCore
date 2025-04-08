603c0e66656f6016445a39b5a111fa52d13ce9b4
Ahmad Rizky Ramadani <dhani.francisco.9@gmail.com>
2025-04-08 21:02:49 +0700

    [BAB 4] Layout

--- App.jsx
+++ App.jsx
@@ -11,7 +11,7 @@ export default function App() {
     <View style={styles.container}>
       {/* Atur warna status bar */}
       <StatusBar style="light" backgroundColor={colors.primary} />
-      <News />
+      <Profile />
     </View>
   );
 }
